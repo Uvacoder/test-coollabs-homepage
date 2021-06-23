db91211e587eb2981d31f271522a7bff88ac456f
Andras Bacsai <andras.bacsai@gmail.com>
2021-06-23 22:34:40 +0200

    first commit

---
+++ src/routes/products.svelte
@@ -0,0 +1,31 @@
+<div class="max-w-sm mx-auto py-16 px-4 sm:px-6 lg:px-8">
+	<div class="text-center">
+		<p
+			class="mt-1  text-4xl font-extrabold text-white sm:text-5xl sm:tracking-tight lg:text-6xl text-center gradient"
+		>
+			Products
+		</p>
+	</div>
+</div>
+<div>
+	<a href="https://coolify.io" class="text-white cursor-pointer w-full flex justify-center py-10">
+		<div
+			class="bg-black rounded-lg transition duration-200 ease-in-out border-4 border-transparent border-dashed hover:border-indigo-500 transform hover:-translate-y-1 hover:scale-110 w-4/5 shadow-xl"
+		>
+			<div class="max-w-7xl mx-auto px-2 py-8 sm:px-6 lg:px-8">
+				<div class="text-center flex flex-col justify-center items-center">
+					<p
+						class="mt-1 pb-8 text-4xl font-extrabold text-white text-5xl sm:tracking-tight lg:text-6xl text-center"
+					>
+						Coolify
+					</p>
+					<h2 class="text-2xl md:text-3xl font-extrabold text-white">
+						An open-source, hassle-free, self-hostable<br class="md:block hidden" />
+						<span class="text-indigo-400">Heroku</span>
+						& <span class="text-green-400">Netlify</span> alternative
+					</h2>
+				</div>
+			</div>
+		</div>
+	</a>
+</div>
