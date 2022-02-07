f4c36e2d5fb77fab2c148e10e4632c5e3f59147d
Andras Bacsai <andras.bacsai@gmail.com>
2022-02-07 23:32:49 +0100

    Added fonts, removed coolTalk for now

--- src/routes/products.svelte
+++ src/routes/products.svelte
@@ -31,6 +31,27 @@
 </div>
 
 <div>
+	<a href="https://github.com/coollabsio/fonts" class="text-white cursor-pointer w-full flex justify-center py-10">
+		<div
+			class="bg-black rounded-lg transition duration-200 ease-in-out border-4 border-transparent border-dashed hover:border-indigo-500 transform hover:-translate-y-1 hover:scale-110 w-4/5 shadow-xl"
+		>
+			<div class="max-w-7xl mx-auto px-2 py-8 sm:px-6 lg:px-8">
+				<div class="text-center flex flex-col justify-center items-center">
+					<p
+						class="mt-1 pb-8 font-extrabold text-white text-5xl sm:tracking-tight lg:text-6xl text-center"
+					>
+						Fonts
+					</p>
+					<h2 class="text-2xl md:text-3xl font-extrabold text-white pt-8">
+						Privacy-focused Google Fonts CDN alternative
+					</h2>
+
+				</div>
+			</div>
+		</div>
+	</a>
+</div>
+<!-- <div>
 	<a href="https://cooltalk.io" class="text-white cursor-pointer w-full flex justify-center py-10">
 		<div
 			class="bg-black rounded-lg transition duration-200 ease-in-out border-4 border-transparent border-dashed hover:border-indigo-500 transform hover:-translate-y-1 hover:scale-110 w-4/5 shadow-xl"
@@ -50,4 +71,4 @@
 			</div>
 		</div>
 	</a>
-</div>
+</div> -->
